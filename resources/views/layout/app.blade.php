7b12f183edd1faa39473e962d2bcaa82f8189ca6
Mukti <mukti.devmail@gmail.com>
2022-09-22 18:38:25 +0700

    Edit Title on Layout Page

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Homepage | {{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
